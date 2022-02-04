497c0fb85f495c4ebe502eab413eb9bbc06e2e92
Neda T <79155257+nedatahe@users.noreply.github.com>
2022-02-04 00:48:39 -0500

    up to 5

--- sql_prescribers-neda.tahe.sql
+++ sql_prescribers-neda.tahe.sql
@@ -1,3 +1,138 @@
-select *
+--1. a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
+
+
+select npi, sum(total_claim_count) as total_claims
+from prescription 
+inner join prescriber 
+USING (npi)
+group by npi, nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
+order by total_claims desc
+limit 1
+   -- b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
+select nppes_provider_first_name, 
+	nppes_provider_last_org_name, 
+	specialty_description, 
+	npi, 
+	sum(total_claim_count) as total_claims
+from prescription 
+inner join prescriber 
+USING (npi)
+group by npi, nppes_provider_first_name, nppes_provider_last_org_name, specialty_description --whatever left in select that is not aggregated on, needs to be on group by
+order by total_claims desc
+limit 1
+
+--2. a. Which specialty had the most total number of claims (totaled over all drugs)?
+select specialty_description, sum(total_claim_count) as claim_sum
+from prescriber 
+inner join prescription
+using (npi)
+group by specialty_description
+order by claim_sum desc
+
+
+
+   -- b. Which specialty had the most total number of claims for opioids?
+
+select specialty_description, sum(total_claim_count) as claim_sum
+from prescriber 
+inner join prescription
+using (npi)
+inner join drug
+using (drug_name)
+where opioid_drug_flag = 'Y'
+group by specialty_description
+order by claim_sum desc
+
+
+    --c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+
+select specialty_description, sum(total_claim_count)
+from prescriber 
+inner join prescription
+using (npi)
+group by specialty_description
+order by sum(total_claim_count)
+
+
+
+
+
+
+   -- d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
+
+--3. a. Which drug (generic_name) had the highest total drug cost?
+select generic_name, sum(total_drug_cost)
 from drug
-limit 5
+inner join prescription
+using (drug_name)
+group by generic_name
+order by sum(total_claim_count) desc
+limit 1
+
+
+
+
+   -- b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
+select generic_name, round(sum(total_drug_cost/total_day_supply), 2)
+from drug
+inner join prescription
+using (drug_name)
+group by generic_name
+order by sum(total_claim_count) desc
+limit 1
+
+
+--4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
+
+select drug_name,
+case 
+when opioid_drug_flag = 'Y' then 'opioid'
+when antibiotic_drug_flag = 'Y' then 'antibiotic'
+else 'neither'
+end as drug_type
+from drug
+order by drug_type asc
+
+
+
+   -- b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
+
+
+
+select drug_name, sum(total_drug_cost)
+case 
+when opioid_drug_flag = 'Y' then 'opioid'
+when antibiotic_drug_flag = 'Y' then 'antibiotic'
+else 'neither'
+end as drug_type
+from drug
+inner join prescription
+group_by sum(total_drug_cost)
+order by drug_type asc
+
+
+--5. a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
+
+    --b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+
+   -- c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
+
+--6. 
+
+   -- a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
+
+  --  b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
+
+   -- c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
+
+--7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid.
+   -- a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Managment') in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. You will likely only need to use the prescriber and drug tables.
+
+  --  b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
+    
+   -- c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+
+
+
+
+
